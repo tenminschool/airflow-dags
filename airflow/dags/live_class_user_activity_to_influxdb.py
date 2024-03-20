86cfa06d21a36d41b7399e9a5e6e6ebb91c0dee5
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-03-20 23:14:25 +0600

    add conf

--- airflow/dags/live_class_user_activity_to_influxdb.py
+++ airflow/dags/live_class_user_activity_to_influxdb.py
@@ -14,20 +14,12 @@ default_args = {
     "retries": 1
 }
 
-liveClassId = "rtfXrc_UO"
-catalogProductId = "rtfXrc_UO"
-catalogSkuId = ""
-programId = "1"
-courseId = "23"
-mediaType = "live_class"
-platform = "k12"
-identificationType = "live_class"
-identificationId = "rtfXrc_UO"
-auth_user_id = "sdfds"
-
 MONGO_DB_NAME = "stage_liveclass_user_activity_db"
 INFLUXDB_BUCKET_NAME = "tracker_stage_db"
 INFLUX_DB_MEASUREMENT = "user_study_duration_logs"
+
+BATCH_SIZE = 100
+DELAY_SEC = 3
 
 options = WriteOptions(
     batch_size=100,
@@ -43,8 +35,47 @@ options = WriteOptions(
 @task()
 def syncMongoDataToInflux(**kwargs):
     print("called")
+
+    liveClassId = ""
+    catalogProductId = ""
+    catalogSkuId = ""
+    programId = ""
+    courseId = ""
+    mediaType = "live_class"
+    platform = ""
+    identificationType = "live_class"
+    identificationId = ""
+
     # print("Remotely received value of {} for key=message".
     # format(kwargs['dag_run'].conf['session_id']))
+
+    conf = kwargs['dag_run'].conf
+    if conf["live_class_id"] is None:
+        raise ValueError("live_class_id is required in conf")
+    liveClassId = conf["live_class_id"]
+
+    if not (conf["catalog_product_id"] is None):
+        catalogProductId = conf["catalog_product_id"]
+
+    if not (conf["catalog_sku_id"] is None):
+        catalogSkuId = conf["catalog_sku_id"]
+
+    if not (conf["program_id"] is None):
+        programId = conf["program_id"]
+
+    if not (conf["couse_id"] is None):
+        couse_id = conf["couse_id"]
+
+    if not (conf["platform"] is None):
+        platform = conf["platform"]
+
+    print("running for liveclass id ", liveClassId)
+    print("catalog product id ", catalogProductId)
+    print("catalog sku id ", catalogSkuId)
+    print("program id ", programId)
+    print("course id ", programId)
+    print("platform ", platform)
+
     mongoHook = MongoHook(mongo_conn_id="stage_mongo_db_connection")
     influxClient = InfluxDBClient(url=Variable.get("INFLUX_DB_URL"),
                                   token=Variable.get("INFLUX_DB_TOKEN"),
@@ -52,7 +83,7 @@ def syncMongoDataToInflux(**kwargs):
 
     pingRes = influxClient.ping()
     if not pingRes:
-        raise Exception("Cannot connect to InfluxDB")
+        raise ValueError("Cannot connect to InfluxDB")
 
     testConnectionRes = mongoHook.connection.test_connection()
     print("test connection ", testConnectionRes)
@@ -73,7 +104,8 @@ def syncMongoDataToInflux(**kwargs):
         playHeadEndAt: datetime = userActivity["leaving_at"]
 
         point = Point.measurement(INFLUX_DB_MEASUREMENT).tag("media_id", liveClassId).tag("auth_user_id",
-                                                                                          auth_user_id).tag(
+                                                                                          userActivity[
+                                                                                              "auth_user_id"]).tag(
             "catalog_product_id", catalogProductId).tag("catalog_sku_id", catalogSkuId).tag("program_id",
                                                                                             programId).tag(
             "course_id", courseId).tag("media_type", mediaType).tag("identification_id", identificationId).tag(
@@ -84,18 +116,18 @@ def syncMongoDataToInflux(**kwargs):
             playHeadStartAt)
         points.append(point)
         count += 1
-        if len(points) == 100:
+        if len(points) == BATCH_SIZE:
             writeAPI = influxClient.write_api(options=SYNCHRONOUS)
-            result = writeAPI.write(INFLUXDB_BUCKET_NAME, org="10MS", record=points)
+            result = writeAPI.write(INFLUXDB_BUCKET_NAME, org=Variable.get("INFLUX_DB_ORG"), record=points)
             points = []
             print("Finished writing ", count, result)
             time.sleep(3)
 
     if len(points) > 0:
         writeAPI = influxClient.write_api(options=SYNCHRONOUS)
-        writeAPI.write(INFLUXDB_BUCKET_NAME, org="10MS", record=points)
+        writeAPI.write(INFLUXDB_BUCKET_NAME, org=Variable.get("INFLUX_DB_ORG"), record=points)
         print("Finished writing ", count)
-        time.sleep(3)
+        time.sleep(DELAY_SEC)
 
 
 with DAG(dag_id="live_class_user_activity_to_influx_db_etl", default_args=default_args,
