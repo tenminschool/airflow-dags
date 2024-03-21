6ff902d26467875765b1c59d322082b019806106
Arnob <arnobxtreme@gmail.com>
2024-03-21 11:33:16 +0600

    test.py script for testing

---
+++ airflow/dags/test.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from airflow.models.dag import DAG
+from airflow.decorators import task
+from airflow.utils.task_group import TaskGroup
+
+from airflow.providers.mongo.hooks.mongo import MongoHook
+
+default_args = {
+    "owner": "Arnob",
+    "start_date": datetime(2024, 2, 21),
+    "retries": 1
+}
+
+
+@task()
+def getSessionData(**kwargs):
+    print("called")
+    print("Remotely received value of {} for key=message".
+          format(kwargs['dag_run'].conf['session_id']))
+    hook = MongoHook(mongo_conn_id="stage_mongo_db_connection")
+    client = hook.get_conn()
+    print(client.list_database_names())
+
+
+with DAG(dag_id="exam_session_dag", default_args=default_args, schedule_interval=None) as dag:
+    getSessionData()
+   
+
+
+
+
+
