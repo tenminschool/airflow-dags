594cda1abfa2ee16b79ec17415cc16b7a845b47e
Masum-Osman <ogkmasum@gmail.com>
2024-03-25 12:13:49 +0600

    adding some codes

--- airflow/dags/superchat_to_influx.py
+++ airflow/dags/superchat_to_influx.py
@@ -12,6 +12,19 @@ default_args = {
 @task()
 def sync_super_chat_data(**kwargs):
     print("called sync_super_chat_data")
+    conf = kwargs['dag_run'].conf
+    live_class_id = conf.get('live_class_id', None)
+    catalog_product_id = conf.get("catalog_product_id", None)
+    catalog_sku_id = conf.get("catalog_sku_id", None)
+    program_id = conf.get("program_id", None)
+    course_id = conf.get("course_id", None)
+    media_type = "live_class"
+    platform = conf.get("platform", None)
+    identification_type = "live_class"
+    identification_id = live_class_id
+
+    print(live_class_id, catalog_product_id, catalog_sku_id, program_id, course_id,
+          media_type, platform, identification_type, identification_id)
 
 with DAG(
         dag_id="superchat_to_influx", 
