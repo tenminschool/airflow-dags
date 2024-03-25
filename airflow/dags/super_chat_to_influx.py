2dc74f94b632fd4ab69fa6035f728641e08b6105
Masum-Osman <ogkmasum@gmail.com>
2024-03-25 12:17:46 +0600

    mudulization

--- airflow/dags/super_chat_to_influx.py
+++ airflow/dags/super_chat_to_influx.py
@@ -10,7 +10,7 @@ default_args = {
 }
 
 @task()
-def sync_super_chat_data(**kwargs):
+def init_syncing_super_chat_data(**kwargs):
     print("called sync_super_chat_data")
     conf = kwargs['dag_run'].conf
     live_class_id = conf.get('live_class_id', None)
@@ -26,8 +26,28 @@ def sync_super_chat_data(**kwargs):
     print(live_class_id, catalog_product_id, catalog_sku_id, program_id, course_id,
           media_type, platform, identification_type, identification_id)
 
+@task()
+def generatePostgresQuery():
+    sql_query = f"""
+    SELECT sessions."createdAt" as start_at, sessions.id, conversation_id, identification_type, identification_id, resolved_at as end_at,
+       thread_id, initiated_member_id, members.auth_user_id, rating_type, rating_value, sessions.status
+    FROM sessions
+    INNER JOIN members ON sessions.initiated_member_id = members.id
+    WHERE sessions.identification_type = 'live_class' AND identification_id = '1naPAnx4w1';
+    """
+
+    # WHERE
+    # sessions.identification_type = 'live_class'
+    # AND
+    # identification_id = '{live_class_id}';
+    print("Executing SQL query:")
+    print(sql_query)
+    return sql_query
+
 with DAG(
-        dag_id="superchat_to_influx", 
+        dag_id="super_chat_to_influx", 
         default_args=default_args, 
         schedule_interval=None) as dag:
-    sync_super_chat_data()
+    init_syncing_super_chat_data()
+    query = generatePostgresQuery()
+    print("Query Printing from DAG: ", query)
