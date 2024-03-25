10b056cec285f1d2cdc71cc580a3ca3264a746d6
Masum-Osman <ogkmasum@gmail.com>
2024-03-25 16:37:48 +0600

    generating influx body

--- airflow/dags/super_chat_to_influx.py
+++ airflow/dags/super_chat_to_influx.py
@@ -8,7 +8,10 @@ from airflow.providers.influxdb.hooks.influxdb import InfluxDBClient, Point
 from influxdb_client.client.write_api import WriteOptions, SYNCHRONOUS
 from airflow.models import Variable
 
+INFLUXDB_BUCKET_NAME = "tracker_stage_db"
 INFLUX_DB_MEASUREMENT = "doubt_solve_records"
+BATCH_SIZE = 100
+DELAY_SEC = 3
 
 default_args = {
     "owner": "Papa Tiger",
@@ -73,44 +76,54 @@ def execute_query_and_fetch_result():
         ping_res = influxClient.ping()
         if not ping_res:
             raise ValueError("Cannot connect to InfluxDB")
-            # tags	liveclass_id	auth_user_id	thread_id	catalog_prod_id	catalog_sku_id	program_id	course_id	platform	status	
-            # fields	session_id	start_at	end_at	resolved_at	rating_type	rating_value																																		
+																																		
         for row in results:
-                start_at = row[0]
-                session_id = row[1]
-                conversation_id = row[2]
-                identification_type = row[3]
-                identification_id = row[4]
-                end_at = row[5]
-                thread_id = row[6]
-                initiated_member_id = row[7]
-                auth_user_id = row[8]
-                rating_type = row[9]
-                rating_value = row[10]
-                status = row[11]
+            start_at = row[0]
+            session_id = row[1]
+            conversation_id = row[2]
+            identification_type = row[3]
+            identification_id = row[4]
+            end_at = row[5]
+            thread_id = row[6]
+            initiated_member_id = row[7]
+            auth_user_id = row[8]
+            rating_type = row[9]
+            rating_value = row[10]
+            status = row[11]
                 
-                point = Point.measurement(INFLUX_DB_MEASUREMENT) \
-                    .tag("liveclass_id", identification_id) \
-                    .tag("auth_user_id", auth_user_id) \
-                    .tag("thread_id", thread_id) \
-                    .tag("catalog_product_id", 100) \
-                    .tag("catalog_sku_id", 100) \
-                    .tag("program_id", 100) \
-                    .tag("course_id", 100) \
-                    .tag("platform", 100) \
-                    .tag("status", status) \
-                    .tag("initiated_member_id", initiated_member_id) \
-                    .tag("conversation_id", conversation_id) \
-                    .field("session_id", session_id) \
-                    .field("start_at", start_at) \
-                    .field("end_at", end_at) \
-                    .field("resolved_at", 100) \
-                    .field("rating_type", rating_type) \
-                    .field("rating_value", rating_value) \
-                    .time(start_at)
+            point = Point.measurement(INFLUX_DB_MEASUREMENT) \
+                .tag("liveclass_id", identification_id) \
+                .tag("auth_user_id", auth_user_id) \
+                .tag("thread_id", thread_id) \
+                .tag("catalog_product_id", 100) \
+                .tag("catalog_sku_id", 100) \
+                .tag("program_id", 100) \
+                .tag("course_id", 100) \
+                .tag("platform", 100) \
+                .tag("status", status) \
+                .tag("initiated_member_id", initiated_member_id) \
+                .tag("conversation_id", conversation_id) \
+                .field("session_id", session_id) \
+                .field("start_at", start_at) \
+                .field("end_at", end_at) \
+                .field("resolved_at", 100) \
+                .field("rating_type", rating_type) \
+                .field("rating_value", rating_value) \
+                .time(start_at)
+            points.append(point)
+            count += 1
+            if len(points) == BATCH_SIZE:
+                writeAPI = influxClient.write_api(options=SYNCHRONOUS)
+                result = writeAPI.write(INFLUXDB_BUCKET_NAME, org=Variable.get("INFLUX_DB_ORG"), record=points)
+                points = []
+                print("Finished writing ", count, result)
+                time.sleep(3)
 
-                points.append(point)
-                count += 1
+        if len(points) > 0:
+            writeAPI = influxClient.write_api(options=SYNCHRONOUS)
+            writeAPI.write(INFLUXDB_BUCKET_NAME, org=Variable.get("INFLUX_DB_ORG"), record=points)
+            print("Finished writing ", count)
+            time.sleep(DELAY_SEC)
 
         logging.info("POINTS:::::::: ", points)
         logging.info("COUNTs:::: ", count)
